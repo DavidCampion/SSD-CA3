915e153b6b5689d09a4dd532b67d0cd8b785e1bc
DavidCampion <D00238516@student.dkit.ie>
2022-04-28 22:07:24 +0100

    styling

--- resources/views/feed/show.blade.php
+++ resources/views/feed/show.blade.php
@@ -1,25 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-4/5 m-auto text-left">
-    <div class="py-15">
-        <h1 class="text-6xl">
-            {{ $post->title }}
-        </h1>
+    <div class="block-wrapper">
+        <div class="block-left">
+            <div class="post-image">
+                <img src="{{ asset('images/' . $post->image_path) }}" alt="">
+            </div>
+        </div>
+        <div class="block-right">
+            <span class="text-pri-color post-user-block">
+                <a class="no-underline hover: underline" href="/user/{{ $post->user->id }}">{{ $post->user->name }}</a>
+            </span>
+            <span class="text-pri-color post-date-block">
+                <a class="no-underline hover: underline">Postd on {{ date('jS M Y', strtotime($post->updated_at)) }}
+            </span>
+            <div >
+                <h1 class="text-pri-color post-title-block">
+                    {{ $post->title }}
+                </h1>
+            </div>
+            <p class="text-pri-color font-light post-desc-block">
+                {{ $post->description }}
+            </p>
+        </div>
     </div>
-</div>
-
-<div class="w-4/5 m-auto pt-20">
-    <span class="text-gray-500">
-        <a class="no-underline hover: underline" href="/user/{{ $post->user->id }}">{{ $post->user->name }}</a>, Postd on {{ date('jS M Y', strtotime($post->updated_at)) }}
-    </span>
-    <div class="post-image">
-        <img src="{{ asset('images/' . $post->image_path) }}" alt="">
-    </div>
-
-    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-        {{ $post->description }}
-    </p>
-</div>
-
 @endsection 
