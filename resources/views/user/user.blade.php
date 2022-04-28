693271423eb364caa1f5d7126489229b1c68dff6
DavidCampion <D00238516@student.dkit.ie>
2022-04-28 13:53:15 +0100

    Finished Page CSS

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -20,31 +20,15 @@
         @endif
     @endforeach
 </div>
-<div class="post-grid">
+<div class="post-grid-page">
     @foreach ($posts as $post)
-        <div class="post-block bg-post-gray">
-            <span class="text-gray-500">
-                <span class="font-bold italic text-pri-color">{{ $post->user->name }}</span>
-            </span>
-            <div class="post-image">
+        <div class="post-block-page bg-post-gray">
+            <div class="post-image-page">
                 <img src="{{ asset('images/' . $post->image_path) }}" alt="">
             </div>
-            <div>
-                <h2 class="text-pri-color font-bold text-5xl pb-4">
-                    {{ $post->title }}
-                </h2>
-                <span class="text-sec-color">
-                    {{ date('jS M Y', strtotime($post->updated_at)) }}
-                </span>
-
-                <p class="text-xl text-sec-color pt-8 pb-10 leading-8 font-light">
-                    {{ $post->description }}
-                </p>
-
-                <a href="/feed/{{ $post->slug }}" class="link-button"></a>
-
+            <div class="editdelete-buttons">
                 @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
-                    <span class="float-right">
+                    <span class="float-left">
                         <a 
                             href="/feed/{{ $post->slug }}/edit"
                             class="text-gray-700 italic hover:text-gray-900 pb-1 border-b-2">
@@ -68,6 +52,9 @@
                         </form>
                     </span>
                 @endif
+            </div>
+            <div>
+                <a href="/feed/{{ $post->slug }}" class="link-button"></a>
             </div>
         </div>    
     @endforeach
